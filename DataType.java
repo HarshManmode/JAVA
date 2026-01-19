4cfef70b6af09785a1e7a177bbed2e55563eae91
HarshManmode <harshmanmode0007@gmail.com>
2026-01-19 20:51:35 +0530

    first commit

---
+++ DataType.java
@@ -0,0 +1,29 @@
+class DataType
+{
+    public static void main(String a[])
+    {
+        int num = 9;
+        byte by = 127;
+        short sh = 32767;
+        long l = 5887l;
+
+        float f = 5.67f;
+        double d = 5.67;
+
+        char c = 'a';
+        
+        boolean b = true;
+
+        System.out.println("Integer value: " + num);
+        System.out.println("Byte value: " + by);
+        System.out.println("Short value: " + sh);   
+        System.out.println("Long value: " + l);
+
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);   
+
+        System.out.println("Character value: " + c);
+
+        System.out.println("Boolean value: " + b);
+    }
+}
